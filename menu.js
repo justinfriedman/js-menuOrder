371f3b547a429965e9aa3ed9efcd05023118cf02
justinfriedman <justinfriedman@justins-mbp.dhcp.wustl.edu>
2016-04-25 18:27:13 -0500

    lab for 4/25

--- menu.js
+++ menu.js
@@ -1,6 +1,6 @@
 // begin with a blank/new order
 var order = 'Your order will include ';
-
+var components = [ "Meat", "Rice", "Beans", "Lettuce", "Cheese", "Pico", "Salsa", "Guac", "Corn", "Sour Cream" ];
 
 // build a function to ask questions
 
@@ -15,18 +15,28 @@ else {
 }
 
 }
+console.log("got here");
 
 // call the QUESTION function with the order options
-question("Meat", "YES");
-question("Rice", "YES");
-question("Beans", "YES");
-question("Lettuce", "YES");
-question("Cheese", "YES");
-question("Pico", "YES");
-question("Salsa", "YES");
-question("Guac", "YES");
-question("Corn", "YES");
-question("Sour cream", "YES");
+// question("Meat", "YES");
+// question("Rice", "YES");
+// question("Beans", "YES");
+// question("Lettuce", "YES");
+// question("Cheese", "YES");
+// question("Pico", "YES");
+// question("Salsa", "YES");
+// question("Guac", "YES");
+// question("Corn", "YES");
+// question("Sour cream", "YES");
+
+var i = 0;
+
+while ( i < components.length) {
+
+  question(components[i], "YES");
+  i = i + 1;
+  console.log("in while loop");
+}
 
 // output their order
 document.write("<p> Order: " + order + "<p>");
